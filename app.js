2e030781eadd426abe642c2c4cdaf05906236113
Yevhen-Karpov <goldblogart@gmail.com>
2021-09-13 11:29:49 +0300

    13.09

--- app.js
+++ app.js
@@ -1,77 +1,82 @@
 const galleryItems = [
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
-    description: 'Hokkaido Flower',
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg",
+    description: "Hokkaido Flower",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-    description: 'Container Haulage Freight',
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
+    description: "Container Haulage Freight",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-    description: 'Aerial Beach View',
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
+    description: "Aerial Beach View",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-    description: 'Flower Blooms',
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
+    description: "Flower Blooms",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-    description: 'Alpine Mountains',
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
+    description: "Alpine Mountains",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-    description: 'Mountain Lake Sailing',
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
+    description: "Mountain Lake Sailing",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-    description: 'Alpine Spring Meadows',
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    description: "Alpine Spring Meadows",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-    description: 'Nature Landscape',
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    description: "Nature Landscape",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-    description: 'Lighthouse Coast Sea',
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    description: "Lighthouse Coast Sea",
   },
 ];
 
 const refs = {
-  imagesContainer: document.querySelector('.gallery'),
-  openModalBtn: document.querySelector('.lightbox'),
+  imagesContainer: document.querySelector(".gallery"),
+  openModalBtn: document.querySelector(".lightbox"),
   closeModalBtn: document.querySelector('[data-action="close-lightbox"]'),
-  
+  modalImage: document.querySelector(".lightbox__image"),
 };
-// console.log(refs.backdrop);
-const galleryEl = galleryItems.reduce((string, galleryItem) => string + `<li class="gallery__item">
+
+// ======================================Создание галереи изображений=======================================================
+
+const galleryEl = galleryItems.reduce(
+  (string, galleryItem) =>
+    string +
+    `<li class="gallery__item">
   <a
     class="gallery__link"
     href=${galleryItem.original}
@@ -83,65 +88,49 @@ const galleryEl = galleryItems.reduce((string, galleryItem) => string + `<li cla
       alt=${galleryItem.description}
     />
   </a>
-</li>`, '')
-refs.imagesContainer.insertAdjacentHTML('afterbegin', galleryEl)
+</li>`,
+  ""
+);
+refs.imagesContainer.insertAdjacentHTML("afterbegin", galleryEl);
 
-refs.imagesContainer.addEventListener('click', onOpenModal);
-refs.closeModalBtn.addEventListener('click', onCloseModal);
-// const backdrop = document.querySelector('.is-open')
-//   console.log(backdrop);
-//   backdrop.addEventListener('click', onBackdropClick)
+// =============================================Открытие и закрытие модального окна===============================================
+
+refs.imagesContainer.addEventListener("click", onOpenModal);
+refs.closeModalBtn.addEventListener("click", onCloseModal);
 
 function onOpenModal(e) {
   e.preventDefault();
-  const condition = e.target.nodeName === 'IMG'
-  console.log(condition);
+  const condition = e.target.nodeName === "IMG";
   if (condition) {
-    window.addEventListener('keydown', onEscKeyPress)
-    refs.openModalBtn.classList.add('is-open')}
-  const modalImage = document.querySelector('.lightbox__image')
-  console.log(modalImage);
+    window.addEventListener("keydown", onEscKeyPress);
+    refs.openModalBtn.classList.add("is-open");
+  }
+
+  refs.modalImage.attributes.src.value = galleryItems.find(
+    (galleryItem) => e.target.attributes.src.value === galleryItem.preview
+  ).original;
 }
 
 function onCloseModal(evt) {
-  window.removeEventListener('keydown', onEscKeyPress)
-  refs.openModalBtn.classList.remove('is-open')
+  window.removeEventListener("keydown", onEscKeyPress);
+  refs.openModalBtn.classList.remove("is-open");
+  refs.modalImage.attributes.src.value = "";
 }
 
-// function onBackdropClick(event) {
-//   if (event.target.classList.contains('lightbox')) {
-//     console.log(event.target.classList.contains('lightbox'));
-//     onCloseModal()
-//   }
-// }
-function onEscKeyPress(event) {
-  if (event.code === 'Escape') {
-    onCloseModal()
+// =========================Закрытие по бэкдропу=====================
+
+const backdrop = document.querySelector(".lightbox");
+backdrop.addEventListener("click", onBackdropClick);
+function onBackdropClick(event) {
+  if (event.target.classList.contains("lightbox__overlay")) {
+    onCloseModal(backdrop);
   }
-
 }
-// const nav = document.querySelector(".js-nav");
 
-// nav.addEventListener("click", handleNavClick);
+// =======================================Закрытие по Escape===================================
 
-// function handleNavClick(event) {
-//   event.preventDefault();
-  
-//   const target = event.target;
-//   console.log("event target: ", target); // посмотрите что тут
-
-//   // Проверяем тип узла, если не ссылка выходим из функции
-//   if (target.nodeName !== "A") return;
-
-//   setActiveLink(target);
-// }
-
-// function setActiveLink(nextActiveLink) {
-//   const currentActiveLink = nav.querySelector("a.active");
-
-//   if (currentActiveLink) {
-//     currentActiveLink.classList.remove("active");
-//   }
-
-//   nextActiveLink.classList.add("active");
-// }
+function onEscKeyPress(event) {
+  if (event.code === "Escape") {
+    onCloseModal();
+  }
+}
