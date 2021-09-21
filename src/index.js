e90d5e5425c998cbc03fa655ee31fc81143c7318
Yevhen-Karpov <goldblogart@gmail.com>
2021-09-21 18:56:21 +0300

    Updates

--- src/index.js
+++
@@ -1,3 +0,0 @@
-import './js/app'
-import './css/styles.scss'
-import './images/icon-close.svg'
--- src/js/refs.js
+++
@@ -1,6 +0,0 @@
-export default {
-  imagesContainer: document.querySelector('.gallery'),
-  openModalBtn: document.querySelector('.lightbox'),
-  closeModalBtn: document.querySelector('[data-action="close-lightbox"]'),
-  modalImage: document.querySelector('.lightbox__image'),
-};
